ebc387dae00d94720f6266176c7bb087916fcf66
anudeep-17 <anudeepsai88@gmail.com>
2021-12-29 22:40:47 -0500

    project first push

---
+++ src/application/Main.java
@@ -0,0 +1,75 @@
+package application;
+	
+import java.util.Random;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	//pre-coded which runs the program,
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+	
+	
+	  
+	@Override
+	
+	public void start(Stage primaryStage) 
+	{
+		try 
+		{
+			primaryStage.setTitle("Algorithm visualizer");
+			
+			BorderPane pane = new BorderPane();
+			
+			pane.setPadding(new Insets(10));
+			
+			
+			//instants the object with all the code of visualizer
+		    visualizer algorithm_visualizer = new visualizer(pane);
+		   
+		    
+		    
+		    Scene scene = new Scene(pane, 800, 600);
+		    
+			//preset
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} 
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+    
+    
+    
+    
+
+    
+    
+}
---
+++ src/application/visualizer.java
@@ -0,0 +1,159 @@
+package application;
+
+import java.util.Random;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+
+public class visualizer 
+{
+
+	visualizer(BorderPane pane)
+	{
+		chartmaker(pane);
+		Objectcreator(pane);
+	}
+
+	
+	
+	
+	
+	//..........................code starts here ................................................
+	
+	 ObservableList<Data<String, Number>> bars;
+	  BarChart<String, Number> barchart1;
+	  FlowPane objectorientator; 
+	  Random rand =  new Random();
+	  
+
+	//chart is made here ------
+    public void chartmaker(BorderPane pane)
+	{
+		CategoryAxis xAxis = new CategoryAxis();
+		NumberAxis yAxis = new NumberAxis();
+		
+	    barchart1 = new BarChart<String, Number>(xAxis, yAxis);
+	   
+
+	    barchart1.setLegendVisible(false);
+        barchart1.getYAxis().setTickLabelsVisible(false);
+        barchart1.getYAxis().setOpacity(0);
+        barchart1.getXAxis().setTickLabelsVisible(false);
+        barchart1.getXAxis().setOpacity(0);
+        barchart1.setHorizontalGridLinesVisible(false);
+        barchart1.setVerticalGridLinesVisible(false);
+        
+       
+       bars = FXCollections.observableArrayList(); //instantiating the bars 
+       fillindata(); // filling data into the bar graph and setting the chart for user.
+       barchart1.getData().add(new Series<>(bars)); //adding them to the barchart
+       
+      
+    
+       pane.setCenter(barchart1);
+       
+	}
+    
+    
+    
+    
+  //object creator method that helps the in creating the buttons and text fields.
+	public void Objectcreator(BorderPane pane)
+	{
+	
+		objectorientator = new FlowPane();
+		objectorientator.setHgap(10);
+		objectorientator.setVgap(10);
+		
+	    Buttoncreate("RESET" , () -> resetvalues(pane));		
+		Buttoncreate("BUBBLESORT", () -> bubbleSort());
+		
+		pane.setBottom(objectorientator);
+		
+	}
+	
+	//button creator method takes name of button and runnable method.
+	private void Buttoncreate(String nameofbutton, Runnable method)
+	{
+		// TODO Auto-generated method stub
+		Button create = new Button(nameofbutton);
+		create.setOnAction(event -> method.run());
+		objectorientator.getChildren().add(create);
+	}
+	
+	
+	
+	
+	
+	
+	
+	//reseting the graph by running with new values.
+	public void resetvalues(BorderPane pane)
+	{
+		chartmaker(pane);
+	}
+	
+	
+	//creating random values and filling array
+	public int randomnum_forgraphing()
+	{
+		return rand.nextInt(100);
+	}
+	
+	
+	
+	//filling data in observable list to work on bar chart in method chartmaker()
+    public void fillindata()
+    {
+    	 for(int i = 0; i < 10; i++)
+		   {
+			   Data<String, Number> dataObject = new Data<>(Integer.toString(i + 1), randomnum_forgraphing());
+			   bars.add(dataObject);
+		   }
+    	 
+    }
+    
+    
+    
+    
+    /*
+     * ---------------------Bubble Sort algorithm----------------------------- 
+     */
+    
+    private void bubbleSort() 
+    {
+        double[] array = bars.stream().mapToDouble(data -> data.getYValue().doubleValue()).toArray();
+
+        double temp;
+        for(int i = 0; i <array.length; i++)
+        {
+            for(int j = 1; j <array.length -i; j++)
+            {
+                if(array[j-1] > array[j])
+                {
+                    temp = array[j -1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                   
+                }
+            }
+        }
+        
+        
+        for (int i = 0; i < array.length; i++) 
+        {
+        	  	
+            bars.get(i).setYValue(array[i]);
+          
+        }
+    }
+	
+}
